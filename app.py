51e5a8c678fabc0905873098b7fb9b0854da9235
mayurakhi14 <mayurakhimaiti877@gmail.com>
2026-04-01 11:27:40 +0530

    Correct status message to 'successFUL'

--- app.py
+++ app.py
@@ -37,7 +37,7 @@ def get_albums():
     response = requests.request("GET", url, headers=headers, data=payload)
     return jsonify({
         "data": response.json(),
-        "status": "success",
+        "status": "successFUL",
         "status_code": 200
     })
 
